9da31459d7fb845a599d912bb5aa59a820d8317c
GPRyan <ryan@grandpad.net>
2019-10-11 01:18:21 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# gcal-zoom-tools
+# Google Calendar Zoom Tools
+
+Hate clicking around just to find the link to join or share to the Zoom meeting your currently apart of? Maybe not, but I do, so I made this.
+
+## Setup
+* Python 3.x
+* Google Calendar Python API Tools (Step 1 and 2 [here](https://developers.google.com/calendar/quickstart/python))
+
+## Usage
+Your meeting started, but you're too lazy to open your calendar or the Zoom app to join the correct one:
+```bash
+python zoom_current_join.py
+```
+
+You're listening in on a meeting via a Zoom room or someone elses computer, and your boss asks you to share your screen. You don't want to join, show your face, and create a bunch of audio feedback in the room. YOU JUST WANT TO SHARE YOUR SCREEN.
+```bash
+python zoom_current_share.py
+```
+
+This was fun.
---
+++ gcal_service.py
@@ -0,0 +1,55 @@
+from __future__ import print_function
+import pickle
+import os.path
+import datetime
+from dateutil import parser
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+
+# If modifying these scopes, delete the file token.pickle.
+SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+
+def get():
+    creds = None
+    # The file token.pickle stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.pickle'):
+        with open('token.pickle', 'rb') as token:
+            creds = pickle.load(token)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.pickle', 'wb') as token:
+            pickle.dump(creds, token)
+
+    return build('calendar', 'v3', credentials=creds)
+
+def current_events():
+    current = []
+
+    now_utc_formatted = datetime.datetime.utcnow().isoformat() + 'Z'
+    events_result = get().events().list(calendarId='primary', timeMin=now_utc_formatted,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+    events = events_result.get('items', [])
+
+    if not events:
+        return current
+
+    for event in events:
+        start = parser.parse(event['start'].get('dateTime'))
+        end = parser.parse(event['end'].get('dateTime'))
+        now_local = datetime.datetime.now(start.tzinfo)
+
+        if start <= now_local <= end:
+            current.append(event)
+
+    return current
